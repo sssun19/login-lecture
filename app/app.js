10d5959b2f771d5403e1348a217224421d4db905
sssun <98_0509@naver.com>
2023-09-06 18:24:55 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// module
+const express = require("express");
+const app = express();
+
+// routing
+const home = require("./src/routes/home");
+
+//app setting
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
+module.exports = app;
+console.log("확인", home);
